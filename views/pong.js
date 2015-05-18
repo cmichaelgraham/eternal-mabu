e680b068604bdb698299e6b49fdba42a900b9218
Eternal-Mabu <kgraham1900@gmail.com>
2015-05-17 22:11:47 -0700

    moving pong

--- views/pong.js
+++ views/pong.js
@@ -1,6 +1,10 @@
 define(["require", "exports"], function (require, exports) {
     var Pong = (function () {
         function Pong() {
+            var _this = this;
+            this.x = 50;
+            this.y = 50;
+            setInterval(function () { _this.x++; }, 1000);
         }
         return Pong;
     })();
--- views/pong.ts
+++ views/pong.ts
@@ -1,1 +1,13 @@
-export class Pong{}
+export class Pong{
+
+  x = 50;
+  y=50;
+
+  constructor(){
+    setInterval(()=>{this.x++;}, 1000);
+
+  }
+
+
+
+}
